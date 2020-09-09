a1477e4352c1b3be423e443e4932016e57cfa5b2
SM <hcooch2ch3@gmail.com>
2020-09-10 01:15:35 +0900

    Initial Commit

---
+++ DaumImageSearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DaumImageSearchApp
+//
+//  Created by 임성민 on 2020/09/10.
+//  Copyright © 2020 SeongMin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
